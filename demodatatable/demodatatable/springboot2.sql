72f5b21397fe4b1a748633f96408fdeaec736bee
kapilpartap <kapilpartap@gmail.com>
2025-04-24 14:56:35 +0530

    Initial db  commit

---
+++ demodatatable/demodatatable/springboot2.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM employee
+LIMIT 0, 1000
+
+-- Date: 2025-04-24 10:47
+*/
+INSERT INTO `` (`id`,`name`,`department`) VALUES (1,'Jane Smith','IT');
+INSERT INTO `` (`id`,`name`,`department`) VALUES (2,'John doe','managing team');
+INSERT INTO `` (`id`,`name`,`department`) VALUES (3,'Rita brenda','IT');
